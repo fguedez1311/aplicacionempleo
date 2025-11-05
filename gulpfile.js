8de22e6fbc6e19e981d1b237f71c98c8d98e85f1
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-11-05 15:21:47 -0400

    Estructura del proyecto en sass

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+import {src,dest,watch} from 'gulp'
+import * as dartSass from 'sass'
+import gulSass from 'gulp-sass'
+
+const sass=gulSass(dartSass)
+
+export function css(done){
+    src('src/scss/app.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(dest('build/css'))
+    done()
+}
+
+export function dev(){
+    watch('src/scss/**/*.scss',css)
+}
