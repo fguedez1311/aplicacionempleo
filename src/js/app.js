44bde2d0a23869a905c05ff0d7256b11d1a2f4e4
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-11-27 07:30:18 -0400

    Delegación de eventos

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,18 @@
-const botones=document.querySelectorAll('.button-apply-job')
-console.log(botones)
-botones.forEach(boton=>{
-    boton.addEventListener('click',()=>{
-        boton.textContent="Aplicado!"
-        boton.classList.add('is-applied')
-        boton.disabled=true
-    })
+// const botones=document.querySelectorAll('.button-apply-job')
+// console.log(botones)
+// botones.forEach(boton=>{
+//     boton.addEventListener('click',()=>{
+//         boton.textContent="Aplicado!"
+//         boton.classList.add('is-applied')
+//         boton.disabled=true
+//     })
 
+// })
+
+const jobsListingSection=document.querySelector('.resultados__jobslistings')
+jobsListingSection.addEventListener('click',function(event){
+    const element=event.target
+    if(element.classList.contains('button-apply-job')){
+        console.log('Es el botón')
+    }
 })
-
