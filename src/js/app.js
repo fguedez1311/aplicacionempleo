047d9aa5f7f33f2617694618a67feec5473c6020
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-12-01 08:15:58 -0400

    feat:Aplicación de clase dinámica

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
-alert("bienvenido desde app.js!!!")
+const filter=document.querySelector("#filter-location")
+const mensaje=document.querySelector('#filter-selected-value')
+const jobs=document.querySelectorAll('.resultados__article')
+filter.addEventListener('change',function(){
+    const seletecValue=filter.value
+    if(seletecValue){
+        mensaje.textContent=`Has Seleccionado: ${seletecValue}`
+        
+    }
+    else{
+        mensaje.textContent=''
+    }
+    jobs.forEach(job=>{
+    const modalidad= job.dataset.modalidad
+    if(seletecValue==='' || seletecValue===modalidad){
+        job.classList.remove('is-hidden')
+    }
+    else{
+       job.classList.add('is-hidden')
+    }
+})
+})
+
+
+
